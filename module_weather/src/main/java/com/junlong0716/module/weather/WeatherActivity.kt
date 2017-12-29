87f216985f3e9856412316cfba006cb9ed695a13
巴黎没有摩天轮 <1056407060@qq.com>
2017-12-29 16:58:03 +0800

    增添 福利模块 天气模块

---
+++ module_weather/src/main/java/com/junlong0716/module/weather/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.junlong0716.module.weather
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
